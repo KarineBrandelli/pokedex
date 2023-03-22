8299ff74669db7e1741440ef04f7d61ce0adb3fb
Karine Brandelli <karinebrandelli@live.com>
2023-03-22 16:37:11 -0300

    build: add Route to Sign In tag

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
 import { UserCircle } from "@phosphor-icons/react";
 
 export default function Header() {
   return (
-    <nav className="bg-blue-800 w-screen h-24 px-10 flex items-center justify-between sticky top-0">
-      <img src="src/assets/pokedex.png" className="h-[85%]"></img>
-      <span className="text-yellow-400 flex items-center gap-4 cursor-pointer">
-        <p className="font-semibold">Log in</p>
+    <nav className="bg-blue-800 min-h-24 px-10 py-3 flex flex-wrap items-center min-[340px]:justify-between justify-center gap-5 sticky top-0">
+      <img src="src/assets/pokedex.png" className="w-[11rem]"></img>
+      <div className="text-yellow-300 flex flex-col items-center gap-1">
         <UserCircle size={45} />
-      </span>
+        <Link to={"/SignIn"}>
+          <p className="font-semibold">Sign In</p>
+        </Link>
+      </div>
     </nav>
   );
 }
