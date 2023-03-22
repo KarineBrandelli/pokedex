ffb986d84327d2a09de0e660b2946ca92cb0e077
Karine Brandelli <karinebrandelli@live.com>
2023-03-22 16:40:00 -0300

    build: create Home page, import and render Header and Card component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Cards from "../components/Cards/Cards";
+import Header from "../components/Header/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Cards />
+    </>
+  );
+}
