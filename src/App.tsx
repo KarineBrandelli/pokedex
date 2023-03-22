25080d646b0fbe1f9c3024cc7c5f0eee9c0385fd
Karine Brandelli <karinebrandelli@live.com>
2023-03-22 16:46:32 -0300

    build: import react router dom and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <>
