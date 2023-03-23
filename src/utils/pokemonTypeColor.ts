7e9b301fd565959e3e03dea5fa6ff00449897cae
Karine Brandelli <karinebrandelli@live.com>
2023-03-23 19:50:29 -0300

    build: create colors utils for each pokemon type

---
+++ src/utils/pokemonTypeColor.ts
@@ -0,0 +1,24 @@
+type Colors = Record<string, string>;
+
+const colors: Colors = {
+  bug: "bg-gradient-to-r from-lime-500 via-lime-400 to-lime-300",
+  water: "bg-gradient-to-r from-sky-500 via-sky-400 to-sky-300",
+  grass: "bg-gradient-to-r from-green-500 via-green-400 to-green-300",
+  ice: "bg-gradient-to-r from-cyan-500 via-cyan-400 to-cyan-300",
+  fire: "bg-gradient-to-r from-red-500 via-red-400 to-red-300",
+  dark: "bg-gradient-to-r from-zinc-500 via-zinc-400 to-zinc-300",
+  fairy: "bg-gradient-to-r from-pink-500 via-pink-400 to-pink-300",
+  steel: "bg-gradient-to-r from-slate-500 via-slate-400 to-slate-300",
+  rock: "bg-gradient-to-r from-emerald-500 via-emerald-400 to-emerald-300",
+  ghost: "bg-gradient-to-r from-violet-500 via-violet-400 to-violet-300",
+  ground: "bg-gradient-to-r from-stone-500 via-stone-400 to-stone-300",
+  poison: "bg-gradient-to-r from-purple-500 via-purple-400 to-purple-300",
+  dragon: "bg-gradient-to-r from-orange-500 via-orange-400 to-orange-300",
+  fighting: "bg-gradient-to-r from-rose-500 via-rose-400 to-rose-300",
+  normal: "bg-gradient-to-r from-neutral-500 via-neutral-400 to-neutral-300",
+  electric: "bg-gradient-to-r from-amber-500 via-amber-400 to-amber-300",
+  flying: "bg-gradient-to-r from-fuchsia-500 via-fuchsia-400 to-fuchsia-300",
+  psychic: "bg-gradient-to-r from-indigo-500 via-indigo-400 to-indigo-300",
+};
+
+export default colors;
