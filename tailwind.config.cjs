df98ec5e3c5847b0850503373b1ac7f6d74cf3a7
Karine Brandelli <karinebrandelli@live.com>
2023-03-23 19:36:20 -0300

    build: remove colors from theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,39 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      blue: colors.blue,
-      yellow: colors.yellow,
-
-      bug: colors.lime,
-      ice: colors.cyan,
-      fire: colors.red,
-      dark: colors.zinc,
-      water: colors.sky,
-      fairy: colors.pink,
-      steel: colors.slate,
-      grass: colors.green,
-      rock: colors.emerald,
-      ghost: colors.violet,
-      ground: colors.stone,
-      poison: colors.purple,
-      dragon: colors.orange,
-      fighting: colors.rose,
-      normal: colors.neutral,
-      electric: colors.amber,
-      flying: colors.fuchsia,
-      psychic: colors.indigo,
-    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
