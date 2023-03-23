607fc345c8129bb70b26440f139673ee0b597b34
Karine Brandelli <karinebrandelli@live.com>
2023-03-23 15:06:54 -0300

    build: import UserProfile component and add a Route to it

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/SignIn" element={<SignIn />} />
           <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/UserProfile" element={<UserProfile />} />
         </Routes>
       </BrowserRouter>
     </>
