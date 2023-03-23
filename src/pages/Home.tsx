8cee57eddffa3050f5b400394f60aedc930feee6
Karine Brandelli <karinebrandelli@live.com>
2023-03-23 19:56:43 -0300

    build: import MenuFilter and Pagination components, add useEffect axios, create list state, add main tag

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,34 @@
-import Cards from "../components/Cards/Cards";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import Card from "../components/Card/Card";
 import Header from "../components/Header/Header";
+import MenuFilter from "../components/MenuFilter/MenuFilter";
+import Pagination from "../components/Pagination/Pagination";
 
 export default function Home() {
+  const [list, setList] = useState<{ name: string; url: string }[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=48")
+      .then((response) => setList(response.data.results));
+  }, []);
+
+  console.log(list);
+
   return (
     <>
       <Header />
-      <Cards />
+      <MenuFilter />
+      <main className="w-[85%] mx-auto">
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-8 mb-16">
+          {list.map((item) => (
+            <Card key={item.name} name={item.name} url={item.url} />
+          ))}
+        </div>
+      </main>
+      <Pagination />
     </>
   );
 }
