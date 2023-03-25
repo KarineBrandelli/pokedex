1814d5a75328355d94a95f6c9b1f27f86dbc5735
Karine Brandelli <karinebrandelli@live.com>
2023-03-25 19:33:36 -0300

    build: create dataGeneration utils to store information about the generations

---
+++ src/utils/dataGeneration.ts
@@ -0,0 +1,60 @@
+const generations = [
+  {
+    id: 1,
+    link: "generation-i",
+    text: "I",
+    offset: 0,
+    limit: 151,
+  },
+  {
+    id: 2,
+    link: "generation-ii",
+    text: "II",
+    offset: 151,
+    limit: 100,
+  },
+  {
+    id: 3,
+    link: "generation-iii",
+    text: "III",
+    offset: 251,
+    limit: 135,
+  },
+  {
+    id: 4,
+    link: "generation-iv",
+    text: "IV",
+    offset: 386,
+    limit: 107,
+  },
+  {
+    id: 5,
+    link: "generation-v",
+    text: "V",
+    offset: 493,
+    limit: 156,
+  },
+  {
+    id: 6,
+    link: "generation-vi",
+    text: "VI",
+    offset: 649,
+    limit: 72,
+  },
+  {
+    id: 7,
+    link: "generation-vii",
+    text: "VII",
+    offset: 721,
+    limit: 88,
+  },
+  {
+    id: 8,
+    link: "generation-viii",
+    text: "VIII",
+    offset: 809,
+    limit: 89,
+  },
+];
+
+export default generations;
