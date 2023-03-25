65ca66bf76b518ab7fd961ed133848f2c9e881d2
Karine Brandelli <karinebrandelli@live.com>
2023-03-25 19:30:19 -0300

    build: add function to previous and next page, set interface PaginationProps

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,11 +1,27 @@
-export default function Pagination() {
+interface PaginationProps {
+  handlePrevious: () => void;
+  handleNext: () => void;
+  setHidden: boolean;
+}
+
+export default function Pagination({
+  handlePrevious,
+  handleNext,
+  setHidden,
+}: PaginationProps) {
   return (
-    <div className="w-[85%] mx-auto font-bold text-lg pb-10">
-      <div className="flex justify-center">
-        <button className="border-[1px] border-white bg-blue-800 hover:bg-blue-600 transition duration-300 ease-out text-white rounded-l-xl py-3 px-5">
-          Previous page
+    <div className={setHidden ? "w-[85%] mx-auto font-bold text-lg" : "hidden"}>
+      <div className="flex justify-center pb-10">
+        <button
+          onClick={handlePrevious}
+          className="border-[1px] border-white bg-blue-800 hover:bg-blue-600 transition duration-300 ease-out text-white rounded-l-xl py-3 px-5"
+        >
+          Previous
         </button>
-        <button className="border-[1px] border-white bg-blue-800 hover:bg-blue-600 transition duration-300 ease-out text-white  rounded-r-xl py-3 px-5">
+        <button
+          onClick={handleNext}
+          className="border-[1px] border-white bg-blue-800 hover:bg-blue-600 transition duration-300 ease-out text-white  rounded-r-xl py-3 px-5"
+        >
           Next
         </button>
       </div>
