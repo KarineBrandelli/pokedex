bb4f9ab4f939147bcb372f4e9ed7cdf0d13a4a96
Karine Brandelli <karinebrandelli@live.com>
2023-03-26 18:00:50 -0300

    build: get information from localStorage and render user name if it's logged

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,19 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { UserCircle } from "@phosphor-icons/react";
 
 export default function Header() {
+  const [loggedUser, setLoggedUser] = useState("");
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("user-info");
+
+    if (storedData) {
+      const loggedUserName = JSON.parse(storedData);
+      setLoggedUser(loggedUserName.name);
+    }
+  }, []);
+
   return (
     <nav className="bg-blue-800 min-h-24 px-10 py-3 flex flex-wrap items-center min-[340px]:justify-between justify-center gap-5 sticky top-0 z-10">
       <a href="/">
@@ -9,9 +21,16 @@ export default function Header() {
       </a>
       <div className="text-yellow-300 flex flex-col items-center gap-1">
         <UserCircle size={45} />
-        <Link to={"/SignIn"}>
-          <p className="font-semibold">Sign In</p>
-        </Link>
+
+        {loggedUser.length > 0 ? (
+          <Link to={"/UserProfile"}>
+            <p className="font-semibold">{loggedUser}</p>
+          </Link>
+        ) : (
+          <Link to={"/UserInfo"}>
+            <p className="font-semibold">Sign In</p>
+          </Link>
+        )}
       </div>
     </nav>
   );
