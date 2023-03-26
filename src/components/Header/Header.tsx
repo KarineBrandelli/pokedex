5dab31a79c6a303e93a92b76869d126f5869d0ee
Karine Brandelli <karinebrandelli@live.com>
2023-03-26 11:53:29 -0300

    build: aff anchor to header image to reload page when clicked

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,9 @@ import { UserCircle } from "@phosphor-icons/react";
 export default function Header() {
   return (
     <nav className="bg-blue-800 min-h-24 px-10 py-3 flex flex-wrap items-center min-[340px]:justify-between justify-center gap-5 sticky top-0 z-10">
-      <img src="src/assets/pokedex.png" className="w-[11rem]"></img>
+      <a href="/">
+        <img src="src/assets/pokedex.png" className="w-[11rem]"></img>
+      </a>
       <div className="text-yellow-300 flex flex-col items-center gap-1">
         <UserCircle size={45} />
         <Link to={"/SignIn"}>
