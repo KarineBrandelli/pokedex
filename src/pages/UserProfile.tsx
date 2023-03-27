0ce71943dbd5e40501b9c142a5931b44ddc7983a
Karine Brandelli <karinebrandelli@live.com>
2023-03-27 10:26:50 -0300

    style: remove display flex from nav, add media query to texts and nav image, adjust pokemons container padding

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -19,7 +19,10 @@ export default function UserProfile({ name, email }: UserInformation) {
     email: "youruser@domain.com",
   });
 
-  const [userPokemons, setUserPokemons] = useState<Array<{ name: string; url: string }>>([{
+  const [userPokemons, setUserPokemons] = useState<
+    Array<{ name: string; url: string }>
+  >([
+    {
       name: "Pikachu",
       url: "https://pokeapi.co/api/v2/pokemon/pikachu",
     },
@@ -62,8 +65,11 @@ export default function UserProfile({ name, email }: UserInformation) {
 
   return (
     <>
-      <nav className="bg-blue-800 px-10 py-3 flex min-[340px]:justify-between justify-center sticky top-0">
-        <img src="src/assets/pokedex.png" className="w-[11rem]"></img>
+      <nav className="bg-blue-800 px-10 py-3 sticky top-0">
+        <img
+          src="src/assets/pokedex.png"
+          className="w-[11rem] max-[375px]:mx-auto"
+        ></img>
       </nav>
       <div className="w-[85%] mx-auto pt-10 pb-5 flex text-blue-600 font-bold">
         <Link to={"/"} className="flex items-center gap-4">
@@ -73,25 +79,25 @@ export default function UserProfile({ name, email }: UserInformation) {
       </div>
 
       <div className="w-[85%] mx-auto">
-        <div className="flex max-[465px]:flex-col max-[465px]:mx-auto items-center gap-5 w-fit py-6 pl-2 pr-4 bg-gradient-to-r from-yellow-400/80 to-yellow-300/80 md:text-lg font-bold rounded-xl">
-          <span className="text-blue-600">
-            <UserCircle size={100} />
+        <div className="flex max-[465px]:flex-col max-[465px]:mx-auto items-center gap-3 max-w-fit p-4 bg-gradient-to-r from-yellow-400/80 to-yellow-300/80 md:text-lg font-bold rounded-xl">
+          <span className="text-blue-600 text-[4rem] sm:text-[6rem]">
+            <UserCircle />
           </span>
           <div className="flex flex-col max-[465px]:items-center gap-3 text-white ">
-            <span className="bg-blue-600 w-fit px-3 py-1 rounded-lg">
-              Name: {userProfileInfo.name}
-            </span>
-            <span className="bg-blue-600 w-fit px-3 py-1 rounded-lg">
-              Email: {userProfileInfo.email}
-            </span>
+            <p className="bg-blue-600 max-w-fit px-3 py-1 rounded-lg flex flex-wrap gap-1 justify-center text-sm sm:text-base">
+              Name: <span>{userProfileInfo.name}</span>
+            </p>
+            <p className="bg-blue-600 max-w-fit px-3 py-1 rounded-lg flex flex-wrap gap-1 justify-center text-sm sm:text-base">
+              Email: <span>{userProfileInfo.email}</span>
+            </p>
           </div>
         </div>
 
-        <h2 className="pt-10 pb-5 max-[465px]:text-center text-2xl md:text-3xl text-blue-600 font-bold">
+        <h2 className="pt-7 pb-5 max-[465px]:text-center text-2xl md:text-3xl text-blue-600 font-bold">
           Your Pokédex!
         </h2>
 
-        <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-8 pb-7">
           {userPokemons.length === 0 ? (
             <div className="bg-gradient-to-r from-blue-800 to-blue-500 flex flex-col gap-5 justify-center items-center text-center rounded-3xl h-64 shadow-lg p-4 md:text-xl text-white font-semibold">
               <p>You don't have any pokemons in your favorites yet.</p>
