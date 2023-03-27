1f13c82fad80beb454452133f442a4d7034cb8d5
Karine Brandelli <karinebrandelli@live.com>
2023-03-27 10:07:04 -0300

    style: remove position absolute and paddings

--- src/components/BackHomeButton/BackHomeButton.tsx
+++ src/components/BackHomeButton/BackHomeButton.tsx
@@ -3,7 +3,7 @@ import { ArrowLeft } from "@phosphor-icons/react";
 
 export default function BackHomeButton() {
   return (
-    <div className="absolute pt-5 pl-1 text-yellow-300">
+    <div className="text-yellow-300">
       <Link to={"/"}>
         <ArrowLeft size={32} />
       </Link>
