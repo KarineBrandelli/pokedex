c4f794134baf3a223919e116134490caffd547bd
Karine Brandelli <karinebrandelli@live.com>
2023-03-27 10:19:14 -0300

    style: change icons size, remove width from input and add font size

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -17,7 +17,7 @@ export default function MenuFilter({
   return (
     <span className="flex items-center justify-between mr-4">
       <MagnifyingGlass
-        size={25}
+        size={23}
         onClick={handleClick}
         className="absolute text-blue-600 ml-3 cursor-pointer"
       />
@@ -26,10 +26,10 @@ export default function MenuFilter({
         value={value}
         placeholder="Search pokemon"
         onChange={handleChange}
-        className="bg-blue-100 border-2 focus:border-blue-600 outline-none focus:outline-none transition focus:duration-500 ease-in-out py-3 pl-12 pr-4 w-[100%] min-[434px]:w-[22rem] rounded-xl"
+        className="bg-blue-100 border-2 focus:border-blue-600 outline-none focus:outline-none transition focus:duration-500 ease-in-out py-3 pl-12 pr-4 min-[434px]:w-[22rem] rounded-xl text-sm sm:text-base"
       />
       <X
-        size={20}
+        size={18}
         onClick={handleClear}
         className="text-blue-600 cursor-pointer -ml-8"
       />
