05d0946942bbb6361b1f9deb5a0f2d4984494c11
Karine Brandelli <karinebrandelli@live.com>
2023-05-18 08:39:06 -0300

    build: useContext to import states and set to onClick functions

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,25 +1,25 @@
+import { useContext } from "react";
+
+import { AppContext } from "../../context/AppContext";
+
 interface PaginationProps {
-  handlePrevious: () => void;
-  handleNext: () => void;
   setHidden: boolean;
 }
 
-export default function Pagination({
-  handlePrevious,
-  handleNext,
-  setHidden,
-}: PaginationProps) {
+export default function Pagination({ setHidden }: PaginationProps) {
+  const { limit, offset, setOffset } = useContext(AppContext);
+
   return (
     <div className={setHidden ? "w-[85%] mx-auto font-bold text-lg" : "hidden"}>
       <div className="flex justify-center pb-10">
         <button
-          onClick={handlePrevious}
+          onClick={() => setOffset(offset - limit)}
           className="border-[1px] border-white bg-blue-800 hover:bg-blue-600 transition duration-300 ease-out text-white rounded-l-xl py-3 px-5"
         >
           Previous
         </button>
         <button
-          onClick={handleNext}
+          onClick={() => setOffset(offset + limit)}
           className="border-[1px] border-white bg-blue-800 hover:bg-blue-600 transition duration-300 ease-out text-white  rounded-r-xl py-3 px-5"
         >
           Next
