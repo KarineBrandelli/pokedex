a6d094455a2385917fb22ca8c7d3f23d8b901c47
Karine Brandelli <karinebrandelli@live.com>
2023-05-18 08:41:27 -0300

    build: add functions to handle input search and import states with AppContext

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -1,36 +1,68 @@
-import { ChangeEvent } from "react";
+import axios from "axios";
+
+import { useContext, ChangeEvent } from "react";
+import { AppContext } from "../../context/AppContext";
+
 import { MagnifyingGlass, X } from "@phosphor-icons/react";
 
-interface InputSearchProps {
-  handleClick: () => void;
-  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  handleClear: () => void;
-  value: string;
-}
+export default function MenuFilter() {
+  const {
+    setList,
+    inputValue,
+    setInputValue,
+    setIsLoading,
+    setIsSearching,
+    limit,
+    offset,
+  } = useContext(AppContext);
 
-export default function MenuFilter({
-  handleClick,
-  value,
-  handleChange,
-  handleClear,
-}: InputSearchProps) {
+  function findPokemonInput() {
+    if (inputValue.length !== 0) {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${inputValue}`)
+        .then((response) => {
+          const { name } = response.data;
+          setList([
+            { name, url: `https://pokeapi.co/api/v2/pokemon/${inputValue}` },
+          ]);
+          setIsSearching(false);
+        })
+        .catch((error) => {
+          alert("Pokemon not found");
+        });
+    }
+  }
+
+  function clearInputValue() {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit${limit}`)
+      .then((response) => {
+        setTimeout(() => setIsLoading(false), 500);
+        setList(response.data.results);
+        setInputValue("");
+        setIsSearching(true);
+      });
+  }
+
   return (
     <span className="flex items-center justify-between mr-4">
       <MagnifyingGlass
         size={23}
-        onClick={handleClick}
+        onClick={findPokemonInput}
         className="absolute text-blue-600 ml-3 cursor-pointer"
       />
       <input
         type="text"
-        value={value}
+        value={inputValue}
         placeholder="Search pokemon"
-        onChange={handleChange}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setInputValue(e.target.value)
+        }
         className="bg-blue-100 border-2 focus:border-blue-600 outline-none focus:outline-none transition focus:duration-500 ease-in-out py-3 pl-12 pr-4 min-[434px]:w-[22rem] rounded-xl text-sm sm:text-base"
       />
       <X
         size={18}
-        onClick={handleClear}
+        onClick={clearInputValue}
         className="text-blue-600 cursor-pointer -ml-8"
       />
     </span>
