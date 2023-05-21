759c6cca51732a662ca3aee13367de968ec9e9ed
Karine Brandelli <karinebrandelli@live.com>
2023-05-21 17:35:22 -0300

    build: remove addPokemon function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,23 +37,6 @@ export function Home() {
       })
   }, [offset])
 
-  function addPokemon(name: string, url: string) {
-    const storedData = localStorage.getItem('user-info')
-
-    if (storedData) {
-      const profilePokemons = JSON.parse(storedData)
-      const userDataPokemons = {
-        ...profilePokemons,
-        pokemons: [...profilePokemons.pokemons, { name, url }],
-      }
-
-      localStorage.setItem('user-info', JSON.stringify(userDataPokemons))
-      alert("You've captured that pokemon!")
-    } else {
-      history('/UserInfo')
-    }
-  }
-
   return (
     <>
       <Header />
