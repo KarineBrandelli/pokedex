caefa927dc7ed37d7905d0034ae8426ce5f7e941
Karine Brandelli Padilha <karine.padilha@ngi.com.br>
2025-04-07 12:28:10 -0300

    poc: add clarity-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { clarity } from 'clarity-js'
+import { useEffect } from 'react'
 import { Route, Routes, HashRouter } from 'react-router-dom'
 
 import { Home } from './pages/Home'
@@ -5,6 +7,26 @@ import { UserInfo } from './pages/UserInfo'
 import { UserProfile } from './pages/UserProfile'
 
 export function App() {
+  useEffect(() => {
+    clarity.consent()
+
+    clarity.set('user_id', '25051995')
+    clarity.set('user_email', 'karinebrandelli@live.com')
+    clarity.set('user_name', 'karine')
+
+    clarity.start({
+      projectId: 'r0hcyguxdv',
+      upload: 'https://m.clarity.ms/collect',
+      track: true,
+      content: true,
+      cookies: ['user_id', 'user_email', 'user_name', 'user_organization'],
+    })
+
+    return () => {
+      clarity.stop()
+    }
+  }, [])
+
   return (
     <>
       <HashRouter>
