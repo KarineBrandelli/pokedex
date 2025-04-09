21072683925b595254913da11f8bac191cccd1e0
Karine Brandelli Padilha <karine.padilha@ngi.com.br>
2025-04-09 12:24:18 -0300

    poc: new relic npm

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,46 @@ import { Home } from './pages/Home'
 import { UserInfo } from './pages/UserInfo'
 import { UserProfile } from './pages/UserProfile'
 
+import { BrowserAgent } from '@newrelic/browser-agent/loaders/browser-agent'
+
+const options = {
+    init: {
+        session_replay: {
+            enabled: true,
+            block_selector: '',
+            mask_text_selector: '*',
+            sampling_rate: 10.0,
+            error_sampling_rate: 100.0,
+            mask_all_inputs: true,
+            collect_fonts: true,
+            inline_images: false,
+            inline_stylesheet: true,
+            fix_stylesheets: true,
+            preload: false,
+            mask_input_options: {},
+        },
+        distributed_tracing: { enabled: true },
+        privacy: { cookies_enabled: true },
+        ajax: { deny_list: ['bam.nr-data.net'] },
+    },
+    info: {
+        beacon: 'bam.nr-data.net',
+        errorBeacon: 'bam.nr-data.net',
+        licenseKey: 'NRJS-9e935e7cfff5f20a5d1',
+        applicationID: '1589067086',
+        sa: 1,
+    },
+    loader_config: {
+        accountID: '6592502',
+        trustKey: '6592502',
+        agentID: '1589067086',
+        licenseKey: 'NRJS-9e935e7cfff5f20a5d1',
+        applicationID: '1589067086',
+    },
+}
+
+new BrowserAgent(options)
+
 export function App() {
   useEffect(() => {
     clarity.consent()
